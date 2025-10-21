662cef65bd3e14710da07aa089e32fb9814fdc1c
Ilja List <il2014@gmx.net>
2025-10-21 15:04:32 +0200

    before post of r/Neovim for julials

--- lua/plugins/mason-workaround.lua
+++ lua/plugins/mason-workaround.lua
@@ -1,6 +1,10 @@
 -- Error message, solution:
 -- https://github.com/LazyVim/LazyVim/issues/6039
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0", enabled = false },
+  {
+    "mason-org/mason.nvim",
+    -- version = "^1.0.0",
+    enabled = true,
+  },
   { "mason-org/mason-lspconfig.nvim", enabled = false, version = "^1.0.0" },
 }
