f183a0bfe7a64557b5a2a7961a2a423cb9e2779c
Ilja List <il2014@gmx.net>
2025-10-21 16:08:09 +0200

    added settings to disable the missingrefs warning

--- init.lua
+++ init.lua
@@ -10,4 +10,52 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 -- use julia language server
-vim.lsp.enable("julials")
+-- vim.lsp.enable("julials")
+
+require("lspconfig").julials.setup({
+  on_new_config = function(new_config, new_root_dir)
+    local server_path = "/home/ilja/.julia/packages/LanguageServer/Fwm1f/src"
+    local cmd = {
+      "julia",
+      "--project=" .. server_path,
+      "--startup-file=no",
+      "--history-file=no",
+      "-e",
+      [[
+          using Pkg
+          Pkg.instantiate()
+          using LanguageServer
+          depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+          project_path = let
+              dirname(something(
+                  ## 1. Finds an explicitly set project (JULIA_PROJECT)
+                  Base.load_path_expand((
+                      p = get(ENV, "JULIA_PROJECT", nothing);
+                      p === nothing ? nothing : isempty(p) ? nothing : p
+                  )),
+                  ## 2. Look for a Project.toml file in the current working directory,
+                  ##    or parent directories, with $HOME as an upper boundary
+                  Base.current_project(),
+                  ## 3. First entry in the load path
+                  get(Base.load_path(), 1, nothing),
+                  ## 4. Fallback to default global environment,
+                  ##    this is more or less unreachable
+                  Base.load_path_expand("@v#.#"),
+              ))
+          end
+          @info "Running language server" VERSION pwd() project_path depot_path
+          server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+          server.runlinter = true
+          run(server)
+        ]],
+    }
+    new_config.cmd = cmd
+  end,
+  settings = {
+    julia = {
+      lint = {
+        missingrefs = "none",
+      },
+    },
+  },
+})
